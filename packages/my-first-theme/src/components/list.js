4990563fbcf73a61fb5f182e868107d43d6d0f9e
Jonathan Asbell <asbelljc@gmail.com>
2022-02-03 22:08:08 -0500

    Added code to list.js to show post excerpts on listing pages.

--- packages/my-first-theme/src/components/list.js
+++ packages/my-first-theme/src/components/list.js
@@ -2,19 +2,29 @@ import React from "react";
 import { connect, styled } from "frontity";
 import Link from "@frontity/components/link";
 
-const List = ({ state, actions }) => {
+const List = ({ state, actions, libraries }) => {
   const data = state.source.get(state.router.link);
+
+  const Html2React = libraries.html2react.Component;
 
   return (
     <Items>
       {data.items.map((item) => {
         const post = state.source[item.type][item.id];
+        const linkTagRegex = /<a[^>]*>([^<]+)<\/a>/g;
 
         return (
-          <Link key={item.id} link={post.link}>
-            {post.title.rendered}
-            <br />
-          </Link>
+          <React.Fragment key={item.id}>
+            <Link link={post.link}>
+              {post.title.rendered}
+              <br />
+            </Link>
+            <Html2React html={post.excerpt.rendered} />
+            <span>
+              <Link link={post.link}>READ MORE</Link>
+            </span>
+            {item !== data.items[data.items.length - 1] ? <hr /> : null}
+          </React.Fragment>
         );
       })}
       <PrevNextNav>
@@ -48,8 +58,26 @@ const Items = styled.div`
     display: block;
     margin: 6px 0;
     font-size: 1.2em;
+    font-weight: 500;
     color: steelblue;
     text-decoration: none;
+  }
+
+  hr {
+    border: 0;
+    height: 1px;
+    margin: 20px 0;
+    background-image: linear-gradient(
+      to right,
+      rgba(0, 0, 0, 0),
+      rgba(0, 0, 0, 0.2),
+      rgba(0, 0, 0, 0)
+    );
+  }
+
+  p {
+    font-size: 14px;
+    font-style: italic;
   }
 `;
 
